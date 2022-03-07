10aa00caf075f170aae5e749dcf73f200a4bb138
Lucas Magno <lucasmagno695@gmail.com>
2022-03-07 12:29:49 -0300

    Commit do desafio Desconto de valores

---
+++ desafioNumber.java
@@ -0,0 +1,29 @@
+package testandoNumeros;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class desafioNumber {
+	public static void main(String[] args) {
+		
+		
+			try (Scanner input = new Scanner(System.in)) {
+				System.out.print("Valor do produto: ");
+				double entradaValor = input.nextDouble();
+				
+				BigDecimal bg = new BigDecimal(entradaValor);
+				bg = bg.multiply(new BigDecimal(0.1));
+
+				DecimalFormat df = new DecimalFormat("R$ #,##0.00"); 
+				String valorFormatado =  df.format(bg);
+				System.out.println("10% de " + "R$ " + entradaValor + " é " + valorFormatado);
+			
+			
+			}catch(InputMismatchException e) {
+				System.out.println("Entrada Inválida, por favor coloque apenas números!");
+			}
+		
+	}
+}
